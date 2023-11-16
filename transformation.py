003fb0df8e508652896627cc7caa59a98d7b4371
limoo <limoodrice@gmail.com>
2023-11-16 16:43:53 +0300

    Scripts to perform aggrations, filtering and loading data to respective tables in rdbms

--- transformation.py
+++ transformation.py
@@ -1,5 +1,6 @@
 from confluent_kafka import Consumer, KafkaError
 from collections import defaultdict
+from datetime import date, timedelta
 import psycopg2
 from psycopg2 import sql
 import json
@@ -7,7 +8,7 @@ import json
 
 def connect_kafka_consumer():
     kafka_config = {
-        'bootstrap.servers': '172.21.0.5:9092',
+        'bootstrap.servers': '172.19.0.6:29092',
         'group.id': 'invoice-group',
         'auto.offset.reset': 'earliest'
     }
@@ -29,9 +30,10 @@ def load_customer_features(cursor, customer_data):
 
 def load_sales_features(cursor, sales_data, invoice_totals):
     #inv, date, amt, crm
+    date_value = date(1970, 1, 1) + timedelta(days=sales_data['date'])
     query = sql.SQL("insert into sales (invoice_number, date, total_amount, crm) values ({},{},{},{});").format(
         sql.Literal(sales_data['invoice_number']),
-        sql.Literal(sales_data['date']),
+        sql.Literal(date_value),
         sql.Literal(invoice_totals[sales_data['invoice_number']]),
         sql.Literal(sales_data['crm'])
     )
@@ -39,26 +41,28 @@ def load_sales_features(cursor, sales_data, invoice_totals):
 
 def load_account_features(cursor, accounts_data, invoice_totals):
     #inv, inv_date, amount, payment, balance
+    date_value = date(1970, 1, 1) + timedelta(days=accounts_data['date'])
     query = sql.SQL("insert into accounts (invoice_number, date, total_amount) values ({},{},{});").format(
         sql.Literal(accounts_data['invoice_number']),
-        sql.Literal(accounts_data['date']),
+        sql.Literal(date_value),
         sql.Literal(invoice_totals[accounts_data['invoice_number']])
     )
     cursor.execute(query)
 
 def load_inventory_track(cursor, inventory_data, product_counts):
     #date, product_code, quantity
+    date_value = date(1970, 1, 1) + timedelta(days=inventory_data['date'])
     query = sql.SQL("insert into inventory (product_name, product_code, quantity, date) values({},{},{},{});").format(
         sql.Literal(inventory_data['product_name']),
         sql.Literal(inventory_data['product_code']),
         sql.Literal(product_counts[inventory_data['product_code']]),
-        sql.Literal(inventory_data['date'])
+        sql.Literal(date_value)
     )
     cursor.execute(query)
 
 def load_logistics_features(cursor, logistic_data, invoice_weights):
     #inv, weight, crm, location
-    query = sql.SQL("insert into logistcis (invoice_number, total_weight, crm, location) values({},{},{},{});").format(
+    query = sql.SQL("insert into logistics (invoice_number, total_weight, crm, location) values({},{},{},{});").format(
         sql.Literal(logistic_data['invoice_number']),
         sql.Literal(invoice_weights[logistic_data['invoice_number']]),
         sql.Literal(logistic_data['crm']),
@@ -87,7 +91,7 @@ def process_kafka_messages(message, cursor, invoice_totals, invoice_weights, pro
         print(f"Error executing SQL query: {e}")
         cursor.connection.rollback()
 
-def kafka_consumer_loop(consumer, cursor):
+def kafka_consumer_loop(consumer, cursor, invoice_totals, invoice_weights, product_counts):
     consumer.subscribe(['postgres.public.invoices'])
     while True:
         msg = consumer.poll(1.0)
@@ -99,7 +103,7 @@ def kafka_consumer_loop(consumer, cursor):
             else:
                 print(msg.error())
                 break
-        process_kafka_messages(msg, cursor)
+        process_kafka_messages(msg, cursor, invoice_totals, invoice_weights, product_counts)
 
 if __name__ == "__main__":
     kafka_consumer = connect_kafka_consumer()
@@ -107,9 +111,10 @@ if __name__ == "__main__":
     postgres_cursor = postgres_conn.cursor()
     invoice_totals = defaultdict(float)
     invoice_weights = defaultdict(float)
+    product_counts = defaultdict(int)
 
     try:
-        kafka_consumer_loop(kafka_consumer, postgres_cursor, invoice_totals, invoice_weights)
+        kafka_consumer_loop(kafka_consumer, postgres_cursor, invoice_totals, invoice_weights, product_counts)
     except KeyboardInterrupt:
         pass
     finally:
