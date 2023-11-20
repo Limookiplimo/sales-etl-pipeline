81ec6d1c29ae15067e4ace7cd1ba549ca8787c5c
limoo <limoodrice@gmail.com>
2023-11-20 15:19:40 +0300

    Changed invoice generation stats

--- generator.py
+++ generator.py
@@ -3,6 +3,7 @@ import pathlib
 import random
 from datetime import datetime
 import psycopg2
+import time
 
 data_path = pathlib.Path(__file__).parent /"data.toml"
 data = tomli.loads(data_path.read_text())
@@ -25,7 +26,7 @@ def load_orders_table(table_name, orders):
 def get_item_count():
     with psycopg2.connect(host="localhost", port=5432, database="etl_pipeline", user="user", password="password") as conn:
         with conn.cursor() as cur:
-            cur.execute(f"select count(*) from invoices")
+            cur.execute(f"select count(*) from transactions")
             count = cur.fetchone()[0]
     return count
 
@@ -82,12 +83,13 @@ def generate_order_products():
 
 if __name__ == "__main__":
     num_orders = 5
-    create_orders_table("invoices", ["customer_name VARCHAR(255)","crm VARCHAR(255)","credit_limit FLOAT","location VARCHAR(255)","order_number VARCHAR(255)","invoice_number INTEGER","line_number INTEGER","date DATE","product_name VARCHAR(255)", "product_code VARCHAR(255)","quantity INTEGER","weight FLOAT","total_weight FLOAT","price FLOAT","total_price FLOAT"])
+    create_orders_table("transactions", ["customer_name VARCHAR(255)","crm VARCHAR(255)","credit_limit FLOAT","location VARCHAR(255)","order_number VARCHAR(255)","invoice_number INTEGER","line_number INTEGER","date DATE","product_name VARCHAR(255)", "product_code VARCHAR(255)","quantity INTEGER","weight FLOAT","total_weight FLOAT","price FLOAT","total_price FLOAT"])
     
     invoice_data = []
     for _ in range(num_orders):
         order_data = generate_order_products()
         invoice_data.extend(order_data)
+        time.sleep(1)
     
-    load_orders_table("invoices", invoice_data)
+    load_orders_table("transactions", invoice_data)
 
