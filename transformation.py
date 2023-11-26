b08ecd576a81169d5f57dfeef8e4e40139717ecf
limoo <limoodrice@gmail.com>
2023-11-26 17:09:13 +0300

    processing kafka records with spark

--- transformation.py
+++ transformation.py
@@ -14,9 +14,9 @@ project_id = credentials.get("project_id")
 client = bigquery.Client(project=project_id)
 
 
-from pyspark.sql.functions import from_json, col, sum, from_unixtime, minute, hour, dayofmonth, weekofyear,month, year
+from pyspark.sql.functions import from_json, col, sum, from_unixtime, dayofmonth, weekofyear,month, year, date_format
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType, FloatType
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
 
 def create_spark_session():
     return SparkSession.builder \
@@ -43,7 +43,8 @@ def schema_define():
         StructField('location', StringType(), True),
         StructField('order_number', StringType(), True),
         StructField('invoice_number', IntegerType(), True),
-        StructField('date', IntegerType(), True),
+        StructField('order_date', IntegerType(), True),
+        StructField('order_time', IntegerType(), True),
         StructField('product_name', StringType(),True), 
         StructField('product_code', StringType(), True),
         StructField('quantity', IntegerType(), True),
@@ -76,21 +77,19 @@ def write_to_bigquery(df, table_name, project_id, dataset_id):
         .awaitTermination()
 
 def sales_fact(df):
-    return df.groupBy("invoice_number", "product_code", "date_timestamp") \
+    return df.groupBy("invoice_number", "product_code", "order_date") \
         .agg(sum("total_price").alias("total_amount"),
-             sum("total_weight").alias("total_weight"))\
-        .withWatermark('date_timestamp', '1 minutes')
+             sum("total_weight").alias("total_weight"))
 
 def time_dim(df):
-    return df.select("date_timestamp", "hour", "minute", "day", "week", "month", "year").distinct()
+    return df.select("order_date", "order_time", "day", "week", "month", "year").distinct()
 
 def customer_dim(df):
     return df.select("crm", "customer_name", "credit_limit", "location").distinct()
 
 def invoice_dim(df):
-    return df.groupBy("invoice_number", "date_timestamp", "crm") \
-        .agg(sum("total_price").alias("total_amount"))\
-        .withWatermark('date_timestamp', '1 minutes')
+    return df.groupBy("invoice_number", "order_date", "crm") \
+        .agg(sum("total_price").alias("total_amount"))
 
 def logistics_dim(df):
     return df.select("invoice_number", "crm", "location").distinct()
@@ -99,35 +98,34 @@ def product_dim(df):
     return df.select("product_name","product_code", "weight").distinct()
 
 def inventory_track_dim(df):
-    return df.groupBy("product_code", "date_timestamp") \
-        .agg(sum("quantity").alias("total_quantity"))\
-        .withWatermark('date_timestamp', '1 minutes')
+    return df.groupBy("product_code", "order_date") \
+        .agg(sum("quantity").alias("total_quantity"))
 
 def main_transformations():
     spark = create_spark_session()
     df = read_kafka_data(spark, "localhost:29092", "postgres.public.transactions")
     parsed_data = parse_kafka_data(df)
     schema = schema_define()
-    df_with_schema = apply_schema(parsed_data, schema)
-    df_with_schema = df_with_schema.withColumn("date_timestamp", from_unixtime(col("date") * 1000000).cast("Timestamp"))
-    df_with_schema = df_with_schema.withColumn("hour", hour(df_with_schema.date_timestamp)) \
-                                   .withColumn("minute", minute(df_with_schema.date_timestamp)) \
-                                   .withColumn("day", dayofmonth(df_with_schema.date_timestamp)) \
-                                   .withColumn("week", weekofyear(df_with_schema.date_timestamp)) \
-                                   .withColumn("month", month(df_with_schema.date_timestamp)) \
-                                   .withColumn("year", year(df_with_schema.date_timestamp))
+    applied_schema = apply_schema(parsed_data, schema)
+    df_with_schema = applied_schema \
+        .withColumn("order_date", from_unixtime(col("order_date") * 86400).cast("date"))\
+        .withColumn("order_time", date_format(from_unixtime(col("order_time") * 100000), "HH:mm:ss"))
+    df_with_schema = df_with_schema.withColumn("day", dayofmonth(df_with_schema.order_date)) \
+                                   .withColumn("week", weekofyear(df_with_schema.order_date)) \
+                                   .withColumn("month", month(df_with_schema.order_date)) \
+                                   .withColumn("year", year(df_with_schema.order_date))
     
     temp_storage_path = "./temporary_storage"
     write_to_temporary_storage(df_with_schema, temp_storage_path)
     batch_data = read_from_temporary_storage(spark, temp_storage_path)
 
-    sales_df = sales_fact(batch_data)
-    time_df = time_dim(batch_data)
-    customer_df = customer_dim(batch_data)
-    invoice_df = invoice_dim(batch_data)
-    logistics_df = logistics_dim(batch_data)
-    product_df = product_dim(batch_data)
-    inventory_df = inventory_track_dim(batch_data)
+    sales_df = sales_fact(df_with_schema)
+    time_df = time_dim(df_with_schema)
+    customer_df = customer_dim(df_with_schema)
+    invoice_df = invoice_dim(df_with_schema)
+    logistics_df = logistics_dim(df_with_schema)
+    product_df = product_dim(df_with_schema)
+    inventory_df = inventory_track_dim(df_with_schema)
 
     sales_df.printSchema()
     sales_df.show(5)
