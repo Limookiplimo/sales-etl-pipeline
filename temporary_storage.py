cf37223070c263a5410ac0b468dde89ef403d2a6
limoo <limoodrice@gmail.com>
2023-11-30 16:54:25 +0300

    refactored script to create temporary storage by introducing functions

--- temporary_storage.py
+++ temporary_storage.py
@@ -1,19 +1,40 @@
 from cassandra.cluster import Cluster
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-# Connect to Cassandra
-cluster = Cluster(['172.20.0.2'])
-session = cluster.connect()
+cassandra_host = [os.environ.get("CASSANDRA_HOST")]
+cassandra_keyspace = os.environ.get("CASSANDRA_KEYSPACE")
 
-# Create keyspace
-keyspace_query = """
-    CREATE KEYSPACE IF NOT EXISTS sales_etl_keyspace
-    WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}
-"""
-session.execute(keyspace_query)
-session.set_keyspace('sales_etl_keyspace')
+SALES_TABLE = "sales"
+TIME_TABLE = "time"
+CUSTOMER_TABLE = "customer"
+INVOICES_TABLE = "invoices"
+LOGISTICS_TABLE = "logistics"
+PRODUCTS_TABLE = "products"
+INVENTORY_TABLE = "inventory_track"
 
-# Create tables
-tables = [
+def connect_to_cassandra(contact_points):
+    cluster = Cluster(contact_points)
+    session = cluster.connect()
+    return cluster, session
+
+def create_keyspace(session, keyspace):
+    keyspace_query = """
+            CREATE KEYSPACE IF NOT EXISTS sales_etl_keyspace
+            WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}
+        """
+    session.execute(keyspace_query)
+    session.set_keyspace(keyspace)
+
+def create_table(session, table_name, table_query):
+    session.execute(table_query)
+
+def intermediate_storage_processes():
+    cluster, session = connect_to_cassandra(cassandra_host)
+    create_keyspace(session, cassandra_keyspace)
+
+    tables = [
     ("sales", """
         CREATE TABLE IF NOT EXISTS sales (
             invoice_number INT PRIMARY KEY,
@@ -79,10 +100,9 @@ tables = [
             PRIMARY KEY (product_code, order_date)
         )
     """)
-]
+    ]
+    for table_name, table_query in tables:
+        create_table(session, table_name, table_query)
 
-for table_name, table_query in tables:
-    session.execute(table_query)
-
-session.shutdown()
-cluster.shutdown()
+    session.shutdown()
+    cluster.shutdown()
