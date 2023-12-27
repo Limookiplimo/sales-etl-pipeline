71df64eceb0ac7eda2dc160ffa2222a642f08576
limoo <limoodrice@gmail.com>
2023-12-27 14:59:49 +0300

    DAG service to orchestrate sales data pipeline

--- dags/piper_dags.py
+++ dags/piper_dags.py
@@ -3,6 +3,7 @@ sys.path.append('~/Documents/Projects/piper/orchestrations')
 
 from job_scripts.generator import load_to_database
 from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 
@@ -12,15 +13,29 @@ default_args = {
     "retry_delay": timedelta(minutes=1),
 }
 dag = DAG(
-    "generate_data",
+    "sales_etl_pipeline",
     default_args=default_args,
     description='Orchestration of data generation, processing and persistence into a data warehouse',
     schedule_interval="5 * * * *"
 )
 task_1 = PythonOperator(
-    task_id='generate_data',
+    task_id='create_sales_data',
     provide_context=True,
     python_callable=load_to_database,
     dag=dag
 )
-task_1
+task_2 = SparkSubmitOperator(
+    task_id='process_sales_data',
+    application='dags/job_scripts/transformation.py',
+    conn_id="spark_default",
+    verbose=False,
+    dag=dag,
+)
+task_3 = SparkSubmitOperator(
+    task_id='sink_to_data_warehouse',
+    application='dags/job_scripts/loading.py',
+    conn_id="spark_default",
+    verbose=False,
+    dag=dag,
+)
+task_1 >> task_2 >> task_3
